d5c397f6b1c83b4f8a063a9b3696c9d36bc974ee
Adam Collier <adamcollierart@gmail.com>
2020-04-25 19:21:59 +0100

    feature: blog post with most recent posts/search sidebar

---
+++ src/templates/blog-post.js
@@ -0,0 +1,144 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import Blogpost from "../components/Blogpost"
+
+import styles from "./blog-post.module.scss"
+import searchIcon from "../icons/search_icon.svg"
+
+const BlogPostTemplate = ({ data, pageContext, location }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const { previous, next } = pageContext
+
+  const allPosts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout
+      location={location}
+      title={siteTitle}
+      container="fluid"
+      className={styles.blogpost}
+    >
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <div className={styles.sidebar}>
+        <div>
+          <div className={styles.bar}>
+            <h4>Articles</h4>
+            <img src={searchIcon} alt="" />
+          </div>
+          <section className={styles.posts}>
+            {allPosts.map(({ node }, i) => (
+              <Blogpost post={node} key={i} noThumbnail />
+            ))}
+          </section>
+        </div>
+      </div>
+      <article className={styles.content}>
+        <header>
+          <h1>{post.frontmatter.title}</h1>
+        </header>
+        {post.frontmatter.featuredImage && (
+          <Image
+            style={{
+              marginBottom: "2rem",
+              borderRadius: "5px",
+            }}
+            sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
+          />
+        )}
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <nav>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </article>
+    </Layout>
+  )
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        featuredImage {
+          childImageSharp {
+            sizes(maxWidth: 720, quality: 90, toFormat: JPG) {
+              ...GatsbyImageSharpSizes_withWebp
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/blog/" } }
+      limit: 3
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            thumbnail {
+              childImageSharp {
+                fixed(width: 114, height: 114, quality: 90, toFormat: JPG) {
+                  ...GatsbyImageSharpFixed_withWebp
+                }
+              }
+            }
+            tags
+          }
+        }
+      }
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+      }
+    }
+  }
+`

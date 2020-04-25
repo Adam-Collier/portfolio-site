67bb0ef876e85c81a9d3d66640bf6aeda16279b6
Adam Collier <adamcollierart@gmail.com>
2020-04-25 19:09:07 +0100

    chore: writing to blog, don't show command if blogpost

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,16 @@ import styles from "./header.module.scss"
 import commandIcon from "../../icons/command_icon.svg"
 
 const Header = () => {
-  console.log(globalHistory.location.pathname)
-  let path =
-    globalHistory.location.pathname === "/"
-      ? "Home"
-      : globalHistory.location.pathname.slice(1).replace(/-/g, " ")
+  let path
+
+  if ((globalHistory.location.pathname.match(/\//g) || []).length === 1) {
+    path =
+      globalHistory.location.pathname === "/"
+        ? "Home"
+        : globalHistory.location.pathname.slice(1).replace(/-/g, " ")
+  } else {
+    path = "Blogpost"
+  }
 
   return (
     <header className={styles.header}>
@@ -19,15 +24,19 @@ const Header = () => {
           Adam Collier
         </Link>
         <h4>{path}</h4>
-        <span>&#183;</span>
-        <div>
-          <img src={commandIcon} alt="command icon" />
-          <h4>{path.slice(0, 1).toUpperCase()}</h4>
-        </div>
+        {path !== "Blogpost" && (
+          <>
+            <span>&#183;</span>
+            <div>
+              <img src={commandIcon} alt="command icon" />
+              <h4>{path.slice(0, 1).toUpperCase()}</h4>
+            </div>
+          </>
+        )}
       </div>
       <div className={styles.navigation}>
         <Link to="/now">Now</Link>
-        <Link to="/writing">Writing</Link>
+        <Link to="/blog">Blog</Link>
         <Link to="/side-projects">Side Projects</Link>
         <Link to="/playground">Playground</Link>
       </div>

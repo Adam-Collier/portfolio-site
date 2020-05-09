4e68b80a09035599da45d12d4081b1c87e01312a
Adam Collier <adamcollierart@gmail.com>
2020-05-09 17:13:36 +0100

    chore: node rather than post

--- src/components/Blogpost/index.js
+++ src/components/Blogpost/index.js
@@ -4,9 +4,9 @@ import Image from "gatsby-image"
 
 import styles from "./blogpost.module.scss"
 
-const Index = ({ post, noThumbnail }) => {
-  const { title, date, description, thumbnail } = post.frontmatter
-  const { slug } = post.fields
+const Index = ({ node, noThumbnail }) => {
+  const { title, date, description, thumbnail } = node.frontmatter
+  const { slug } = node.fields
 
   return (
     <Link
@@ -22,7 +22,7 @@ const Index = ({ post, noThumbnail }) => {
           <h3>{title}</h3>
           <p
             dangerouslySetInnerHTML={{
-              __html: description || post.excerpt,
+              __html: description || node.excerpt,
             }}
           />
         </section>

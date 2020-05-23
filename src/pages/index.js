9f310633ccbb6fbc9fde0db97a694f18319f8a56
Adam Collier <adamcollierart@gmail.com>
2020-05-23 18:56:44 +0100

    feature: simple homepage using clamp for responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styles from "./index.module.scss"
+
+import twitterIcon from "../icons/twitter.svg"
+import spotifyIcon from "../icons/spotify.svg"
+import letterboxdIcon from "../icons/letterboxd.svg"
+
 const IndexPage = props => (
-  <Layout>
+  <Layout className={styles.home}>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h4>
+      Hey there{" "}
+      <span role="img" aria-label="waving emoji">
+        👋
+      </span>{" "}
+      I'm
+    </h4>
+    <h1>Adam Collier</h1>
+    <h2>A UX lovin' Creative Designer &#x26; Developer</h2>
+    <p>
+      This site is as much for me as it is for you, it acts as a collection of
+      thoughts, aha moments and tidbits of code which always seem to escape my
+      little brain somehow. Have a cheeky gander and hit me up with anything...
+      literally anything I’ve got time atm... (film recommendations, books and
+      music are always encouraged).
+    </p>
+    <ul>
+      <li>
+        <a href="https://twitter.com/CollierAdam">
+          <img src={twitterIcon} alt="twitter icon" />
+        </a>
+      </li>
+      <li>
+        <a href="https://open.spotify.com/user/1134435866?si=BeBEyLrhQUWPx0hijLZoZA">
+          <img src={spotifyIcon} alt="spotify icon" />
+        </a>
+      </li>
+      <li>
+        <a href="https://letterboxd.com/mistapolnareff/">
+          <img src={letterboxdIcon} alt="letterboxd icon" />
+        </a>
+      </li>
+    </ul>
   </Layout>
 )
 

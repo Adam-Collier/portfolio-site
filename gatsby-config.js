b6c76d41c7ea9562ec4de57ef7f379103c94850f
Adam Collier <adamcollierart@gmail.com>
2020-06-27 19:47:15 +0100

    chore: added offset via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,12 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-autolink-headers`,
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `80`,
+            },
+          },
           `copy-snippet`,
           `gatsby-remark-prismjs`,
           {

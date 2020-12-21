7274982ba6f0256ad9d8265964cfff3f62343c8e
Adam Collier <adamcollierart@gmail.com>
2020-12-21 11:11:56 +0000

    fix: fixed type issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,10 +9,11 @@ function getInitialColorMode() {
   const persistedColorPreference = window.localStorage.getItem(
     'colorPreference'
   );
-  const hasPersistedPreference = typeof persistedColorPreference === 'boolean';
+  const hasPersistedPreference = typeof persistedColorPreference === 'string';
   // If the user has explicitly chosen light or dark,
   // let's use it. Otherwise, this value will be null.
   if (hasPersistedPreference) {
+    localStorage.setItem('colorPreference', persistedColorPreference);
     return persistedColorPreference;
   }
   // If they haven't been explicit, let's check the media
@@ -20,10 +21,13 @@ function getInitialColorMode() {
   const mql = window.matchMedia('(prefers-color-scheme: dark)');
   const hasMediaQueryPreference = typeof mql.matches === 'boolean';
   if (hasMediaQueryPreference) {
-    return mql.matches ? 'dark' : 'light';
+    const preference = mql.matches ? 'dark' : 'light';
+    localStorage.setItem('colorPreference', preference);
+    return preference;
   }
   // If they are using a browser/OS that doesn't support
   // color themes, let's default to 'light'.
+  localStorage.setItem('colorPreference', 'light');
   return 'light';
 }
 

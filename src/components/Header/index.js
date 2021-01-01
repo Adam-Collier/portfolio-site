5d81fb81534f3013de80c9db395b316344a051f0
Adam Collier <adamcollierart@gmail.com>
2021-01-01 11:50:29 +0000

    fix: fixed flash from useMediaQuery

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -41,9 +41,7 @@ const Header = ({ location, className, isClose, onClick }) => {
               />
               Adam Collier
             </Link>
-            {useMediaQuery('(min-width: 768px)') && location && (
-              <Breadcrumb location={location} />
-            )}
+            {location && <Breadcrumb location={location} />}
           </div>
           <div className={styles.headerRight}>
             <Navigation className={styles.navigation} />

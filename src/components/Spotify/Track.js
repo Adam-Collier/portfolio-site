48295c50b84b0f47d381af4376e4c76116a629ff
Adam Collier <adamcollierart@gmail.com>
2021-01-18 17:02:01 +0000

    feature: Spotify content added

---
+++ src/components/Spotify/Track.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { ArrowUpRight } from 'react-feather';
+import styles from './track.module.css';
+
+const Track = ({ track }) => {
+  const { id, artists, name, album, external_urls } = track;
+  const { images } = album;
+  const { spotify: trackUrl } = external_urls;
+
+  const artistNames = artists.map((artist) => artist.name).join(', ');
+
+  return (
+    <a
+      className={styles.track}
+      key={id}
+      href={trackUrl}
+      target="__blank"
+      rel="noopener noreferrer"
+    >
+      <ArrowUpRight className={styles.arrow} size={18} />
+      <img src={images[1].url} alt="" />
+      <div>
+        <p className={styles.trackName}>{name}</p>
+        <p className={styles.artist}>{artistNames}</p>
+      </div>
+    </a>
+  );
+};
+
+export default Track;

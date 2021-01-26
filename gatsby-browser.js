69926454b73be4358f8321db9e892ade2bc4fc0a
Adam Collier <adamcollierart@gmail.com>
2021-01-26 19:19:46 +0000

    feature: wrap page element with layout component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,8 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 import React from 'react';
+import Layout from './src/components/Layout';
 import { StateProvider } from './src/context';
 
 export const wrapRootElement = ({ element }) => (
   <StateProvider>{element}</StateProvider>
+);
+
+export const wrapPageElement = ({ element, props }) => (
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Layout {...props}>{element}</Layout>
 );

65e5b09d8c15379396546c9df077458beba3ca2c
Adam-Collier <adamcollierart@gmail.com>
2021-02-17 08:56:10 +0000

    feature: dependencies added, use Date rather than git log

--- updateFrontmatter.js
+++ updateFrontmatter.js
@@ -1,28 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const fs = require('fs').promises;
 const matter = require('gray-matter');
-const { execSync } = require('child_process');
 
 const updateFrontmatter = async () => {
   const [, , ...mdFilePaths] = process.argv;
 
   mdFilePaths.forEach(async (path) => {
-    const gitDateUpdated = execSync(
-      `git log -1 --pretty=format:%aI ${path}`
-    ).toString();
-
     const file = matter.read(path);
     const { data: currentFrontmatter } = file;
 
-    const updatedFrontmatter = {
-      ...currentFrontmatter,
-      updatedDate: gitDateUpdated,
-    };
-
-    file.data = updatedFrontmatter;
-
-    const updatedFileContent = matter.stringify(file);
-    fs.writeFile(path, updatedFileContent);
+    if (currentFrontmatter.published === true) {
+      const updatedFrontmatter = {
+        ...currentFrontmatter,
+        updatedDate: new Date().toISOString(),
+      };
+      file.data = updatedFrontmatter;
+      const updatedFileContent = matter.stringify(file);
+      fs.writeFile(path, updatedFileContent);
+    }
   });
 };
 

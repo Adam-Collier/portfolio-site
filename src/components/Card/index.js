70502a90671dd1335aaa805fae837d540bba4ab2
Adam Collier <adamcollierart@gmail.com>
2021-03-15 21:23:45 +0000

    chore: remove Card component

--- src/components/Card/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './card.module.css';
-
-const Card = ({ children, className }) => (
-  <div className={`${styles.card} ${className || ''}`}>{children}</div>
-);
-
-export default Card;

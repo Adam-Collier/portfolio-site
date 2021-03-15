f7923f050c6496b14d4b86e6eade019002b60301
Adam Collier <adamcollierart@gmail.com>
2021-03-15 21:33:54 +0000

    chore: reduce track number and remove intro

--- src/components/Spotify/index.js
+++ src/components/Spotify/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Track from './Track';
-import { intro } from './track.module.css';
 
 const TopTracks = () => {
   const { allTopTracks } = useStaticQuery(graphql`
     {
-      allTopTracks(limit: 10) {
+      allTopTracks(limit: 5) {
         edges {
           node {
             id
@@ -33,17 +32,9 @@ const TopTracks = () => {
     }
   `);
 
-  return (
-    <>
-      <p className={intro}>
-        Wondering what music I’m loving right now? Here’s my top tracks from
-        Spotify.
-      </p>
-      {allTopTracks.edges.map(({ node }, index) => (
-        <Track track={node} key={index} />
-      ))}
-    </>
-  );
+  return allTopTracks.edges.map(({ node }, index) => (
+    <Track track={node} key={index} />
+  ));
 };
 
 export default TopTracks;

a38de7501a28df4e146a2f8891c3863dbdc91521
Adam Collier <adamcollierart@gmail.com>
2021-06-01 17:23:30 +0100

    chore: site config lives here

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Adam Collier',
+  author: {
+    name: 'Adam Collier',
+    summary: 'Creative UX Designer/Developer',
+  },
+  siteUrl: 'https://adamcollier.co.uk',
+  description:
+    'This site is as much for me as it is for you. See what snippets I like to use, what resources I learn from and my thoughts and processes.',
+  social: {
+    twitter: 'collieradam',
+  },
+  openGraph: {
+    // anything in the public directory can be accessed from the root
+    image: '/images/meta-image.jpg',
+  },
+};

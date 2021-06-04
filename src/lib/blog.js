55cabc5aba75a0ce006006d2c103a2457550a5bf
Adam Collier <adamcollierart@gmail.com>
2021-06-04 09:19:28 +0100

    feature: grab mdx content

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -1,25 +1,76 @@
 // Install gray-matter and date-fns
 import matter from 'gray-matter';
-import { parseISO, format } from 'date-fns';
-import fs from 'fs';
 import { join } from 'path';
+// import { parseISO, format } from 'date-fns';
+import fs from 'fs';
 
-// Add markdown files in `src/content/blog`
-const postsDirectory = '/_posts';
+// get the root directory
+const root = process.cwd();
 
-export function getPostBySlug(slug) {
-  const realSlug = slug.replace(/\.md$/, '');
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-  const { data, content } = matter(fileContents);
-  const date = format(parseISO(data.date), 'MMMM dd, yyyy');
+// export const getPostSlugs = () => {
+//   const allDirs = fs.readdirSync(join(root, '_posts'), { withFileTypes: true });
 
-  return { slug: realSlug, frontmatter: { ...data, date }, content };
-}
+//   // remove the hidden files, extract the title and turn into a slug
+//   allDirs.flatMap((dir) => {
+//     // if the dir isn't a directory return remove from the array
+//     if (dir.isDirectory()) return [];
+//     const [year, month, day, ...fileName] = dir.name.split('-');
 
-export function getAllPosts() {
-  const slugs = fs.readdirSync(postsDirectory);
-  const posts = slugs.map((slug) => getPostBySlug(slug));
+//     const slug = fileName.join('-').toLowerCase();
 
-  return posts;
-}
+//     return slug;
+//   });
+// };
+
+// we use the filenames here because our slugs differ from our file names
+export const getContentBySlug = (baseDir, name, fields = []) => {
+  // read the mdx file inside the directory
+  // not the nicest but we are joining our path together here
+  const fileContent = fs.readFileSync(
+    join(root, baseDir, name, 'index.mdx'),
+    'utf-8'
+  );
+
+  // grab the frontmatter using graymatter
+  const { data, content } = matter(fileContent);
+
+  // create the slug
+  const slug = name.toLowerCase();
+  // replace the dashes with spaces
+  const spacedName = name.replace(/-/g, ' ');
+  // capitalise the first letter
+  const title = spacedName.charAt(0).toUpperCase() + spacedName.slice(1);
+
+  // bring all of the data together
+  const postData = {
+    ...data,
+    slug,
+    title,
+    content,
+  };
+
+  const items = {};
+
+  // return only the fields needed
+  fields.forEach((field) => {
+    if (postData[field]) {
+      items[field] = postData[field];
+    }
+  });
+
+  return items;
+};
+
+export const getAllContentOfType = async (contentBaseDir, fields = []) => {
+  // lets grab all of the post directories
+  // using withFileTypes returns and array of dirents
+  const allPostDirs = fs.readdirSync(join(root, contentBaseDir), {
+    withFileTypes: true,
+  });
+
+  return allPostDirs.flatMap((postDir) => {
+    if (postDir.isFile()) return [];
+    const contentDirName = postDir.name;
+    return getContentBySlug(contentBaseDir, contentDirName, fields);
+  });
+};

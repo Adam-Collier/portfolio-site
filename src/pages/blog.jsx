a5f5485b0a35d2e83d21deeebf5119d5bfca6959
Adam Collier <adamcollierart@gmail.com>
2021-06-04 09:21:05 +0100

    feature: list of blogposts render

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,3 +1,17 @@
-const Blog = () => <h1>This is the blog page</h1>;
+import Blogpost from '../components/Blogpost';
+import { getAllContentOfType } from '../lib/blog';
+
+const Blog = ({ posts }) => posts.map((post) => <Blogpost {...post} />);
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllContentOfType('_posts', [
+    'title',
+    'date',
+    'slug',
+    'description',
+  ]);
+
+  return { props: { posts } };
+}

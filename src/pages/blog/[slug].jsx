0b48dbcd2ba5ade9ef651c28650978141b0e7edb
Adam Collier <adamcollierart@gmail.com>
2021-06-08 20:40:26 +0100

    feature: next-mdx-remote setup working with components in scope

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,28 +1,60 @@
-import { getAllContentOfType, getContentBySlug } from '../../lib/blog';
+import fs from 'fs';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import matter from 'gray-matter';
+import dynamic from 'next/dynamic';
+import { join } from 'path';
+import CodeBlock from '../../components/CodeBlock';
 
-const Post = ({ post }) => {
+import { getAllContentOfType } from '../../lib/blog';
+
+const components = {
+  Button: dynamic(() => import('../../components/Button')),
+  FormNoStyles: dynamic(() =>
+    import(
+      '../../../_posts/add-a-form-to-your-Gatsby-site-using-Staticforms/components/FormNoStyles'
+    )
+  ),
+  FormWithStyles: dynamic(() =>
+    import(
+      '../../../_posts/add-a-form-to-your-Gatsby-site-using-Staticforms/components/FormWithStyles'
+    )
+  ),
+  Film: dynamic(() => import('../../components/Film')),
+  FilmCover: dynamic(() => import('../../components/FilmCover')),
+  Grid: dynamic(() => import('../../components/Grid')),
+  pre: (props) => <CodeBlock {...props} />,
+};
+
+const Post = ({ post, source }) => {
   console.log(post, 'this is the post prop');
-  return <>{post.content}</>;
+  return <MDXRemote {...source} components={components} />;
 };
 
 export default Post;
 
-export async function getStaticProps({ custom, params }) {
-  console.log(custom, 'these are the params');
-  const post = getContentBySlug('_posts', params.slug, [
-    'title',
-    'publishedOn',
-    'updatedOn',
-    'slug',
-    'description',
-    'content',
-  ]);
+export async function getStaticProps({ params }) {
+  // get the root directory
+  const root = process.cwd();
+
+  const filePath = join(root, '_posts', params.slug, 'index.mdx');
+  const source = fs.readFileSync(filePath);
+
+  const { content, data } = matter(source);
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  });
 
   return {
     props: {
-      post: {
-        ...post,
-      },
+      source: mdxSource,
+      frontMatter: data,
     },
   };
 }

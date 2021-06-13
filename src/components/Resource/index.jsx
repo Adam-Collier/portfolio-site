6fa2db43b642136be0a9182bbcf8198fc6c65e28
Adam Collier <adamcollierart@gmail.com>
2021-06-13 16:31:12 +0100

    chore: use Stack component

--- src/components/Resource/index.jsx
+++ src/components/Resource/index.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import Link from 'next/link';
 import { ArrowRight } from 'react-feather';
 import Text from '../Text';
+import Stack from '../Stack';
 
 import s from './resource.module.css';
 
 const Resource = ({ title, description, slug }) => (
   <Link href={`/resources/${slug}`}>
     <a className={s.resource}>
-      <article key={slug} aria-label={title}>
-        <div>
+      <Stack as="article" gap={0.5} aria-label={title}>
+        <Stack direction="row" gap={0.5} align="center">
           <Text as="h3" size="lg" heading>
             {title}
           </Text>
           <ArrowRight size={16} />
-        </div>
+        </Stack>
         <Text>{description}</Text>
-      </article>
+      </Stack>
     </a>
   </Link>
 );

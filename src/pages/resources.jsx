3c16053eec507d90957f64a383737b9a4fb04f9c
Adam Collier <adamcollierart@gmail.com>
2021-06-13 21:51:06 +0100

    feature: render all resources and search

---
+++ src/pages/resources.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Resource from '../components/Resource';
+import Text from '../components/Text';
+import Stack from '../components/Stack';
+import Search from '../components/Search';
+import { getAllContentOfType } from '../lib/blog';
+
+const Blog = ({ allResources }) => {
+  const [resources, setResources] = useState(allResources);
+
+  return (
+    <Stack maxWidth="sm" gap={2} style={{ marginTop: '3.5rem' }} padding page>
+      <Text size="lg">
+        This is a group of resources I have either learned something from or
+        thought could become useful in the future.
+      </Text>
+      <Search allData={allResources} setState={setResources} name="resources" />
+      <Stack gap={0.5}>
+        {resources.map((resource) => (
+          <Resource {...resource} />
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Blog;
+
+export async function getStaticProps() {
+  const allResources = await getAllContentOfType('_resources', [
+    'title',
+    'slug',
+    'description',
+    'content',
+  ]);
+
+  return { props: { allResources } };
+}

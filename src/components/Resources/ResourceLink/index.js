b37266dbd6c8922f16c2b95ee4133ce0de36d14c
Adam-Collier <adamcollierart@gmail.com>
2021-06-27 17:16:27 +0100

    chore: comment out for now

--- src/components/Resources/ResourceLink/index.js
+++ src/components/Resources/ResourceLink/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-import { ArrowRight } from 'react-feather';
+// import { ArrowRight } from 'react-feather';
 
-import styles from './resource-link.module.css';
+// import styles from './resource-link.module.css';
 
-const index = ({ node, currentPageId }) => {
-  const { id } = node;
-  const { title } = node.frontmatter;
-  const { slug } = node.fields;
+// const index = ({ node, currentPageId }) => {
+const index = () => (
+  // const { id } = node;
+  // const { title } = node.frontmatter;
+  // const { slug } = node.fields;
 
-  return (
-    <div />
-    // <Link
-    //   to={slug}
-    //   className={`${styles.resourceLink} ${
-    //     currentPageId === id ? styles.active : ''
-    //   }`}
-    //   aria-label={title}
-    // >
-    //   <article key={slug}>
-    //     <h3>{title}</h3>
-    //     {currentPageId === id ? <ArrowRight size={16} /> : null}
-    //   </article>
-    // </Link>
-  );
-};
-
+  <div />
+  // <Link
+  //   to={slug}
+  //   className={`${styles.resourceLink} ${
+  //     currentPageId === id ? styles.active : ''
+  //   }`}
+  //   aria-label={title}
+  // >
+  //   <article key={slug}>
+  //     <h3>{title}</h3>
+  //     {currentPageId === id ? <ArrowRight size={16} /> : null}
+  //   </article>
+  // </Link>
+);
 export default index;

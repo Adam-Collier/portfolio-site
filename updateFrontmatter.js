d07dd98fbabb01cfd16bb8e9dfbbdcb8fceb09fb
Adam-Collier <adamcollierart@gmail.com>
2021-07-02 17:37:58 +0100

    chore: comment out for now

--- updateFrontmatter.js
+++ updateFrontmatter.js
@@ -3,22 +3,20 @@ const fs = require('fs').promises;
 const matter = require('gray-matter');
 
 const updateFrontmatter = async () => {
-  const [, , ...mdFilePaths] = process.argv;
-
-  mdFilePaths.forEach(async (path) => {
-    const file = matter.read(path);
-    const { data: currentFrontmatter } = file;
-
-    if (currentFrontmatter.published === true) {
-      const updatedFrontmatter = {
-        ...currentFrontmatter,
-        updatedOn: new Date().toISOString(),
-      };
-      file.data = updatedFrontmatter;
-      const updatedFileContent = matter.stringify(file);
-      fs.writeFile(path, updatedFileContent);
-    }
-  });
+  // const [, , ...mdFilePaths] = process.argv;
+  // mdFilePaths.forEach(async (path) => {
+  //   const file = matter.read(path);
+  //   const { data: currentFrontmatter } = file;
+  //   if (currentFrontmatter.published === true) {
+  //     const updatedFrontmatter = {
+  //       ...currentFrontmatter,
+  //       updatedOn: new Date().toISOString(),
+  //     };
+  //     file.data = updatedFrontmatter;
+  //     const updatedFileContent = matter.stringify(file);
+  //     fs.writeFile(path, updatedFileContent);
+  //   }
+  // });
 };
 
 updateFrontmatter();

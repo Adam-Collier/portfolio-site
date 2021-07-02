b727471b8d5762adf5079ae67e2d6490648c0b93
Adam Collier <adamcollierart@gmail.com>
2021-07-02 17:28:14 +0100

    feature: top prop for sticky positioning

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,8 +1,13 @@
 import Stack from '../Stack';
 import s from './sidebar.module.css';
 
-const Sidebar = ({ children }) => (
-  <Stack as="aside" gap={1.45} className={s.sidebar}>
+const Sidebar = ({ children, top }) => (
+  <Stack
+    as="aside"
+    gap={1.45}
+    className={s.sidebar}
+    style={{ '--top': top ? `${top}rem` : `0` }}
+  >
     {children}
   </Stack>
 );

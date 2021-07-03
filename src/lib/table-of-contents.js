acc9a3e4a3838177570856349ead838bec6d65ca
Adam-Collier <adamcollierart@gmail.com>
2021-07-03 18:44:40 +0100

    chore: create nested heading structure

--- src/lib/table-of-contents.js
+++ src/lib/table-of-contents.js
@@ -5,22 +5,39 @@ export const getHeadings = (source) => {
     .split('\n')
     .filter((line) => line.match(/^###*\s/));
 
+  // an array for creating our nested structure
+  const headings = [];
+  // a list of headings for our observer
+  // this saves us having to walk the nested structure we make here
+  const headingsList = [];
+
+  let baseLevel;
   // Transform the string '## Some text' into an object
   // with the shape '{ text: 'Some text', level: 2 }'
-  return headingLines.map((raw) => {
-    // remove any hashes or non alphanumeric characters
-    const title = raw.replace(/^###*\s/, '');
-    // make lower case, remove any non alphanumeric characters (apart from spaces)
+  headingLines.forEach((line, index) => {
+    const title = line.replace(/^###*\s/, '');
+    // make lower case, remove any non alphanumeric characters (apart from spaces and dashes)
     // and then add dashes
     const id = title
       .toLowerCase()
-      .replace(/[^\w\s]/gi, '')
+      .replace(/[^a-zA-Z0-9 -]/gi, '')
       .replace(/ /g, '-');
     // I only care about h2 and h3.
     // If I wanted more levels, I'd need to count the
     // number of #s.
-    const level = raw.slice(0, 3) === '###' ? 3 : 2;
+    const level = line.slice(0, 3) === '###' ? 3 : 2;
+    // the first heading becomes our base level
+    // this will decide whether we nest or not
+    if (index === 0) baseLevel = level;
+    // base our headings structure off of the first level obtained
+    if (level === baseLevel) {
+      headings.push({ id, title, items: [] });
+    } else {
+      headings[headings.length - 1].items.push({ id, title });
+    }
 
-    return { id, title, level };
+    headingsList.push({ id });
   });
+
+  return { headings, headingsList };
 };

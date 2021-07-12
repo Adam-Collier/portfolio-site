c7b933a194059dad052cf1c2c561148f6a74751d
Adam-Collier <adamcollierart@gmail.com>
2021-07-12 09:55:54 +0100

    chore: conditional structure depending on viewport width

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,21 @@
+import { Fragment } from 'react';
 import Stack from '../Stack';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 import s from './sidebar.module.css';
 
-const Sidebar = ({ children, top }) => (
-  <Stack
-    as="aside"
-    gap={1.45}
-    className={s.sidebar}
-    style={{ '--top': top ? `${top}vh` : `6rem` }}
-  >
-    {children}
-  </Stack>
-);
+const Sidebar = ({ children, top }) => {
+  const Component = useMediaQuery('(max-width: 767px)') ? Fragment : Stack;
+
+  return (
+    <Component
+      as="aside"
+      gap={1.45}
+      className={s.sidebar}
+      style={{ '--top': top ? `${top}vh` : `6rem` }}
+    >
+      {children}
+    </Component>
+  );
+};
 
 export default Sidebar;

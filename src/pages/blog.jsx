7060c6a7ee6eebe62ec3fc3d00efb2f968d205ad
Adam-Collier <adamcollierart@gmail.com>
2021-07-12 09:57:51 +0100

    chore: wrap in Stack

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,26 +1,23 @@
 import Blogpost from '../components/Blogpost';
 import Page from '../components/Page';
-import Stack from '../components/Stack';
 import Text from '../components/Text';
 import { getAllContentOfType } from '../lib/blog';
 
 const Blog = ({ posts }) => (
-  <Page padding>
-    <Stack maxWidth="sm" gap={1.45} padding page>
-      <Text as="h1" size="2xl" heading>
-        Blog
-      </Text>
-      <Text>
-        This is my attempt at writing blog things. Writing isn't one of my
-        strengths so bear with my poor sentence structures and inevitable typos.
-        However, I'll always try my best! (queue anime hero montage) I'll mostly
-        write content relating to the projects/side projects I'm working on and
-        no doubt there will be some random ones in there too.
-      </Text>
-      {posts.map((post, index) => (
-        <Blogpost {...post} key={index} />
-      ))}
-    </Stack>
+  <Page padding maxWidth="sm" gap={1.45}>
+    <Text as="h1" size="2xl" heading>
+      Blog
+    </Text>
+    <Text>
+      This is my attempt at writing blog things. Writing isn't one of my
+      strengths so bear with my poor sentence structures and inevitable typos.
+      However, I'll always try my best! (queue anime hero montage) I'll mostly
+      write content relating to the projects/side projects I'm working on and no
+      doubt there will be some random ones in there too.
+    </Text>
+    {posts.map((post, index) => (
+      <Blogpost {...post} key={index} />
+    ))}
   </Page>
 );
 

045b26bb2888164682da8f912c6bc3cd1fbf5d92
Adam Collier <adamcollierart@gmail.com>
2021-07-13 16:38:13 +0100

    refactor: resources list

--- src/pages/resources/[slug].jsx
+++ src/pages/resources/[slug].jsx
@@ -7,6 +7,7 @@ import Page from '../../components/Page';
 import Stack from '../../components/Stack';
 import Form from '../../components/Form';
 import Sidebar from '../../components/Sidebar/index.jsx';
+import SharePost from '../../components/SharePost';
 import { getAllContentOfType } from '../../lib/blog';
 import { prepareMDX } from '../../lib/mdx';
 import { baseComponents } from '../../lib/base-components';
@@ -16,45 +17,28 @@ import s from './resource.module.css';
 
 const resource = css.global`
   .resource {
-    display: flex;
-    flex-wrap: wrap;
+    --columns: 200px 1fr;
+    --gap: 1.25rem;
+    display: grid;
+    grid-template-columns: var(--columns);
+    grid-gap: var(--gap);
   }
-  .resource div {
-    flex: 0 0 200px;
-  }
-  @media (max-width: 767px) {
-    .resource div {
-      flex: 1 0 200px;
-    }
-  }
-  @media (min-width: 768px) and (max-width: 1024px) {
-    .resource div {
-      flex: 0 0 150px;
-    }
-  }
-  .resource > p {
-    flex: 1 1 200px;
-  }
-  @media (min-width: 768px) {
-    .resource > p {
-      padding-left: 1.25rem;
-    }
-  }
-  .resource div p {
-    margin-bottom: 0.125rem;
+  .resource div p + p {
+    margin-top: 0.125rem;
     color: var(--foreground-high);
-  }
-  .resource div p:last-of-type {
     font-size: 0.875rem;
   }
-  @media (min-width: 768px) {
-    .resource div p:last-of-type {
-      margin-bottom: 1.45rem;
+
+  @media (max-width: 767px) {
+    .resource {
+      --columns: 100%;
+      --gap: 0;
+      grid-template-rows: min-content min-content;
     }
   }
 `;
 
-const Resource = ({ source, title, quickLinks }) => (
+const Resource = ({ slug, source, title, quickLinks }) => (
   <Page layout="grid" padding>
     <Stack maxWidth="sm" gap={1.45} style={{ gridArea: 'content' }}>
       <style jsx global>
@@ -69,7 +53,7 @@ const Resource = ({ source, title, quickLinks }) => (
         text="Do you know a resource that could benefit another reader and is relevent for this page? Let me know by leaving a short message below and I will take a look!"
       />
     </Stack>
-    <Sidebar top={23}>
+    <Sidebar top={10} style={{ gridArea: 'quick-links' }}>
       <Stack gap={1.45} className={s.quickLinks}>
         <Text size="md" heading>
           <LinkIcon size={14} style={{ marginRight: '4px' }} /> Quick Links
@@ -78,7 +62,7 @@ const Resource = ({ source, title, quickLinks }) => (
           {quickLinks.map((link, index) => (
             <li key={index}>
               <Link href={`/resources/${link.slug}`}>
-                <a>
+                <a className={slug === link.slug ? s.active : ''}>
                   <Text size="sm">{link.title}</Text>
                 </a>
               </Link>
@@ -86,6 +70,7 @@ const Resource = ({ source, title, quickLinks }) => (
           ))}
         </ul>
       </Stack>
+      <SharePost text="Share this resource!" />
     </Sidebar>
   </Page>
 );
@@ -97,23 +82,16 @@ export async function getStaticProps({ params }) {
   const mdx = await prepareMDX(slug, '_resources');
   const allResourceSlugs = await getAllContentOfType('_resources', ['slug']);
 
-  const quickLinks = allResourceSlugs.reduce((acc, curr) => {
-    if (curr.slug === slug) return acc;
-
-    acc.push({
-      slug: curr.slug,
-      title: toTitleCase(curr.slug),
-    });
-
-    return acc;
-  }, []);
-
-  console.log(quickLinks);
+  const quickLinks = allResourceSlugs.map((resourceSlug) => ({
+    slug: resourceSlug.slug,
+    title: toTitleCase(resourceSlug.slug),
+  }));
 
   const title = toTitleCase(slug);
 
   return {
     props: {
+      slug,
       ...mdx,
       title,
       quickLinks,

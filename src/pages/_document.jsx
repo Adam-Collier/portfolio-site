a30c9a33df3c583373e5ae7c609664fd0cf69a22
Adam Collier <adamcollierart@gmail.com>
2021-07-13 16:36:59 +0100

    feature: umami tracking script

---
+++ src/pages/_document.jsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              async
+              defer
+              data-website-id="5e5ce6b5-3fb0-49eb-ae22-ab3cb1fa483b"
+              src="https://umami-production-7v-m.up.railway.app/umami.js"
+            />
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

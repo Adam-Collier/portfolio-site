bc421a183d9d6538fe9f69d6d2626b1ec2380db0
Adam Collier <adamcollierart@gmail.com>
2021-07-14 21:29:30 +0100

    feature: mobile menu closing logic

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useTransition, animated } from 'react-spring';
 import { useContext } from '../../context';
 
@@ -9,6 +9,12 @@ import s from './mobilemenu.module.css';
 
 const MobileMenu = ({ isMobileMenu }) => {
   const dispatch = useContext()[1];
+  // if we close the menu on the same page we transition it out
+  // otherwise we just remove it straight away
+  const [transitionOut, setTransitionOut] = useState(true);
+
+  // get a reference to the menu element here
+  const menuRef = useRef(null);
 
   const transitions = useTransition(isMobileMenu, null, {
     from: {
@@ -17,23 +23,43 @@ const MobileMenu = ({ isMobileMenu }) => {
     enter: {
       maxHeight: '500px',
     },
-    leave: {
-      maxHeight: '0px',
-    },
+    leave: transitionOut ? { maxHeight: '0px' } : { opacity: 0 },
   });
+
+  // if transitionOut is false make sure to reset it after useTransition has used it
+  if (transitionOut === false) setTransitionOut(true);
+
+  // taken from here: https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setTransitionOut(false);
+        dispatch({ type: 'isMobileMenu' });
+      }
+    };
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef, dispatch]);
 
   return transitions.map(
     ({ item, key, props }) =>
       item && (
-        <animated.div className={s.menu} key={key} style={props}>
+        <animated.div className={s.menu} key={key} style={props} ref={menuRef}>
           <>
-            <Header
-              className={s.header}
-              isClose
-              onClick={() => dispatch({ type: 'isMobileMenu' })}
-            />
+            <Header className={s.header} isClose />
             {/* pass in custom styles and change the font size */}
-            <Nav className={s.nav} size="lg" />
+            <Nav
+              className={s.nav}
+              size="lg"
+              onClick={() => {
+                setTransitionOut(false);
+                dispatch({ type: 'isMobileMenu' });
+              }}
+            />
           </>
         </animated.div>
       )

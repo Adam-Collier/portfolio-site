0f5f5625d4db98a0c7077017d18281d3c17d80ad
Adam <adamcollierart@gmail.com>
2021-08-29 21:00:12 +0100

    Notion render table (#86)
    * feature: tables now rendered from Notion
    
    * fix: value one level deep now

--- src/lib/get-headings.js
+++ src/lib/get-headings.js
@@ -2,14 +2,14 @@ export const getHeadings = (blocks) => {
   const headings = [];
 
   blocks.forEach((block) => {
-    if (block.type === 'sub_header') {
-      const subHeaderTitle = block.properties.title[0][0];
+    if (block.value.type === 'sub_header') {
+      const subHeaderTitle = block.value.properties.title[0][0];
       const id = subHeaderTitle.toLowerCase().replace(/ /g, '-');
       headings.push({ id, title: subHeaderTitle, items: [] });
     }
 
-    if (block.type === 'sub_sub_header') {
-      const subSubHeaderTitle = block.properties.title[0][0];
+    if (block.value.type === 'sub_sub_header') {
+      const subSubHeaderTitle = block.value.properties.title[0][0];
       const id = subSubHeaderTitle.toLowerCase().replace(/ /g, '-');
       headings[headings.length - 1].items.push({
         id,

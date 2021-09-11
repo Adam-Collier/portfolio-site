f8d3443c8755be78c9064299bb7a97cf5c2afd5c
Adam-Collier <adamcollierart@gmail.com>
2021-09-11 14:22:29 +0100

    feature: analyze bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -10,4 +14,4 @@ module.exports = {
   images: {
     domains: ['i.scdn.co', 'a.ltrbxd.com', 'www.notion.so', 'mosaic.scdn.co'],
   },
-};
+});
